152d3ec5c6a93a41fc7784b9f3e6a02d56fd91dc
Anastasiya McElnikova <sav20043+McElnikova@yandex.ru>
2023-07-22 00:46:55 +0300

    В основном сделано. Наполняю контентом файл с описанием функций

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,23 +8,39 @@ fetch(url)
     text=text.replaceAll("\r\n","\n");//Приводим перенос строки к виду linux
     text=text.split("\n\n");
     let tbodyHTML="";
+    let smallScreenHTML="";
     text.forEach(element => {
         let trowData=element.trim().split("\n");
         if(trowData.length==3){
-        tbodyHTML+=`
-                    <tr>
-                        <td class="td-method">
-                            ${trowData[0]}
-                        </td>
-                        <td class="td-descript">
-                            ${trowData[1]}
-                        </td>
-                        <td>
-                            ${trowData[2]}
-                        </td>
-                    </tr>
-        `;}
+            tbodyHTML+=`
+                        <tr>
+                            <td class="td-method">
+                                ${trowData[0]}
+                            </td>
+                            <td class="td-descript">
+                                ${trowData[1]}
+                            </td>
+                            <td class="td-example">
+                                ${trowData[2]}
+                            </td>
+                        </tr>
+            `;
+
+            smallScreenHTML+=`
+                        <div class="meth-block_row">
+                            <div class="td-method">
+                                ${trowData[0]}
+                            </div>
+                            <div class="td-descript">
+                                ${trowData[1]}
+                            </div>
+                            <div class="td-example">
+                                ${trowData[2]}
+                            </div>
+                        </div>
+            `;
+        }
     });
-    let tbody=document.getElementById('meth-list');
-    tbody.insertAdjacentHTML("afterbegin",tbodyHTML);
+    document.getElementById('meth-list').insertAdjacentHTML("afterbegin",tbodyHTML);
+    document.getElementById('meth-block-list').insertAdjacentHTML("afterbegin",smallScreenHTML)
 });
