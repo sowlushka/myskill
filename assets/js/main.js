bf168f70e101e079ef8a9d2c2d6b05c23f1aeca0
Anastasiya McElnikova <sav20043+McElnikova@yandex.ru>
2023-07-22 16:57:27 +0300

    Добавлены стили в таблицу для удобного просмотра Добавлен контент функций js Добавлена возможность группировки методам по их назначению

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,31 +11,49 @@ fetch(url)
     let smallScreenHTML="";
     text.forEach(element => {
         let trowData=element.trim().split("\n");
-        if(trowData.length==3){
+        if(trowData.length>2){
+            let indxShift=0;//Сдвиг в индексе массива, когда комментария к группе методов нет
+            let methodGroupName="";
+            let methodGroupNameInBlock="";
+            if(trowData[0].indexOf("/*")>=0 && trowData[0].indexOf("*/")>0){
+            /*Имеется комментарий к группе методов */
+                indxShift=1;//Сдвиг в индексе массива когда имеется комментарий
+                methodGroupName=trowData[0].replace("/*","").replace("*/","").trim();
+                methodGroupName=`
+                        <tr>
+                            <td colspan="3" class="td-methods-group-name">${methodGroupName}</td>
+                        </tr>
+                `;
+                methodGroupNameInBlock=`
+                        <div class="block-methods-group-name">${methodGroupName}</div>
+                `;
+            }
             tbodyHTML+=`
+                        ${methodGroupName}
                         <tr>
                             <td class="td-method">
-                                ${trowData[0]}
+                                ${trowData[indxShift+0]}
                             </td>
                             <td class="td-descript">
-                                ${trowData[1]}
+                                ${trowData[indxShift+1]}
                             </td>
                             <td class="td-example">
-                                ${trowData[2]}
+                                ${trowData[indxShift+2]}
                             </td>
                         </tr>
             `;
 
             smallScreenHTML+=`
+                        ${methodGroupNameInBlock}
                         <div class="meth-block_row">
                             <div class="td-method">
-                                ${trowData[0]}
+                                ${trowData[indxShift+0]}
                             </div>
                             <div class="td-descript">
-                                ${trowData[1]}
+                                ${trowData[indxShift+1]}
                             </div>
                             <div class="td-example">
-                                ${trowData[2]}
+                                ${trowData[indxShift+2]}
                             </div>
                         </div>
             `;
